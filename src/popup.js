0ba4562e78b971e84bbf7d3288abf1e9cc9dc8d2
cyber-mamba <shindojin@outlook.com>
2023-12-10 04:51:18 +0000

    default extension app created.

---
+++ src/popup.js
@@ -0,0 +1,9 @@
+import { HELLO } from './constants.js';
+ 
+  const $body = document.querySelector('body');
+  const $p = document.createElement('p');
+  
+$p.innerHTML = `${HELLO} Popup`;
+  if ($body) {
+    $body.appendChild($p);
+  }
